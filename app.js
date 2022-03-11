b470d780cd7ad4c233eda30a2073078b7522baf9
frmi <frmi2018@gmail.com>
2022-03-11 18:14:45 +0100

    02-connection à la base de données MongoDB

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require("express");
-const app = express();
+
 require("dotenv").config();
 
+// express app
+const app = express();
+
+// db
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => console.log("DB CONNECTED"));
+}
+
+// routes
 app.get("/", (req, res) => {
   res.send("hello from node");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "Page not Found" });
 });
 
 const port = process.env.PORT || 8000;
