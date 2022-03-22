57005fcf2d3c5e37cab32523aef3387dceb68abb
frmi <frmi2018@gmail.com>
2022-03-22 19:51:11 +0100

    Products by sell and arrival on request query params

--- controllers/product.js
+++ controllers/product.js
@@ -141,19 +141,19 @@ exports.update = (req, res) => {
 
 /*
  * sell / arrival
- * by sell = /products?sortBy=sold&order=desc&llimit=4
- * by arrival = /products?sortBy=createdAt&order=desc&llimit=4
+ * by sell = /products?sortBy=sold&order=desc&limit=4
+ * by arrival = /products?sortBy=createdAt&order=desc&limit=4
  * if no params are sent, then all products are returned
  */
 
 exports.list = (req, res) => {
   let order = req.query.order ? req.query.order : "asc";
   let sortBy = req.query.sortBy ? req.query.sortBy : "_id";
-  let limit = req.query.limit ? req.query.limit : 6;
+  let limit = req.query.limit ? parseInt(req.query.limit) : 6;
 
   Product.find()
     .select("-photo")
-    .popullate("category")
+    .populate("category")
     .sort([[sortBy, order]])
     .limit(limit)
     .exec((err, products) => {
